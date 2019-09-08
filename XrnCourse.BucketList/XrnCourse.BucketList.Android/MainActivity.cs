c98c634edc92c521595327e841784ddba292f3cc
sigged <sderdeyn@gmail.com>
2019-09-09 01:58:12 +0200

    add app icons and name for Android project

--- XrnCourse.BucketList/XrnCourse.BucketList.Android/MainActivity.cs
+++ XrnCourse.BucketList/XrnCourse.BucketList.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace XrnCourse.BucketList.Droid
 {
-    [Activity(Label = "XrnCourse.BucketList", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Bucket List", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
