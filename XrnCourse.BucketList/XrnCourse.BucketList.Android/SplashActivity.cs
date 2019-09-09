b47c6431fa44f5692621486c505347ebeb53bf86
sigged <sderdeyn@gmail.com>
2019-09-09 09:48:07 +0200

    add splash screen for Android project

---
+++ XrnCourse.BucketList/XrnCourse.BucketList.Android/SplashActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace XrnCourse.BucketList.Droid
+{
+    [Activity(Label = "Bucket List", Icon = "@drawable/icon", Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            Log.Debug("BucketList", "Performing some startup work that takes a bit of time.");
+            await Task.Delay(500); // Simulate a bit of startup work.
+            Log.Debug("BucketList", "Startup work is finished - starting MainActivity.");
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
