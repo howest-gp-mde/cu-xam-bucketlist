a59e8a792f5d1b2c74d90f7c14f6a1401a3d31b9
sigged <sderdeyn@gmail.com>
2019-09-10 22:09:14 +0200

    create organised folder structure in shared project

---
+++ XrnCourse.BucketList/XrnCourse.BucketList/Constants.cs
@@ -0,0 +1,20 @@
+﻿namespace XrnCourse.BucketList
+{
+    /// <summary>
+    /// Contains various constant values use throughout the application in one place
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Classifies constant values used for Mocking purproses
+        /// </summary>
+        public static class Mocking
+        {
+            /// <summary>
+            /// Delay in milliseconds to simulate long running service requests
+            /// </summary>
+            public const int FakeDelay = 1000;
+        }
+    }
+
+}
