eab8e73718f1c345c426bdcb704dae2c8c01b54e
sigged <sderdeyn@gmail.com>
2019-09-12 16:00:00 +0200

    add bucket list form using StackLayout
    (cherry picked from commit 176bf41921a7b9b080accfaec8ec33c4d4834c88)

--- XrnCourse.BucketList/XrnCourse.BucketList/Views/BucketPage.xaml.cs
+++ XrnCourse.BucketList/XrnCourse.BucketList/Views/BucketPage.xaml.cs
@@ -44,8 +44,45 @@ namespace XrnCourse.BucketList.Views
         protected async override void OnAppearing()
         {
             settings = await settingsService.GetSettings();
+            LoadBucketState();  //fill controls with currentBucket properties
             base.OnAppearing();
         }
 
+        private async void BtnSave_Clicked(object sender, EventArgs e)
+        {
+            SaveBucketState();
+            if (isNew)
+            {
+                currentBucket.Id = Guid.NewGuid(); // generate ID for this bucket
+                await bucketsService.AddBucketList(currentBucket);
+            }
+            else
+            {
+                await bucketsService.UpdateBucketList(currentBucket);
+            }
+            await DisplayAlert("Saved", $"Your bucket list {currentBucket.Title} has been saved", "Ok");
+        }
+
+        /// <summary>
+        /// Load values from currentBucket into controls
+        /// </summary>
+        private void LoadBucketState()
+        {
+            txtTitle.Text = currentBucket.Title;
+            txtDescription.Text = currentBucket.Description;
+            swIsFavorite.IsToggled = currentBucket.IsFavorite;
+            lblPercentComplete.Text = currentBucket.PercentCompleted.ToString("P0");
+        }
+
+        /// <summary>
+        /// Retrieve values from controls and store them in currentBucket
+        /// </summary>
+        private void SaveBucketState()
+        {
+            currentBucket.Title = txtTitle.Text;
+            currentBucket.Description = txtDescription.Text;
+            currentBucket.IsFavorite = swIsFavorite.IsToggled;
+            currentBucket.OwnerId = settings.CurrentUserId;
+        }
     }
 }
