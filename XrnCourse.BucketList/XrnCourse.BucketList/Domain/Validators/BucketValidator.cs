054eec574f17245ef72051e8eea532037af4fd7b
sigged <sderdeyn@gmail.com>
2019-09-12 17:00:10 +0200

    implement a validator for the Bucket type using FluentValidation

---
+++ XrnCourse.BucketList/XrnCourse.BucketList/Domain/Validators/BucketValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using XrnCourse.BucketList.Domain.Models;
+
+namespace XrnCourse.BucketList.Domain.Validators
+{
+    public class BucketValidator : AbstractValidator<Bucket>
+    {
+        public BucketValidator()
+        {
+            RuleFor(bucket => bucket.Title)
+                .NotEmpty()
+                .WithMessage("Title cannot be empty")
+                .Length(3, 30)
+                .WithMessage("Length must be between 3 and 30");
+
+            RuleFor(bucket => bucket.Description)
+                .NotEqual(b => b.Title)
+                .WithMessage("Must be different than Title")
+                .NotEmpty()
+                .WithMessage("Description cannot be empty");
+        }
+    }
+}
