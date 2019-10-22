3a7d3d5b223fc07eecf7deeff1efe69e0961c76e
sigged <sderdeyn@gmail.com>
2019-10-22 21:44:14 +0200

    Add BucketItem CRUD capabilities
    (cherry picked from commit 55f80a6ff593684fbf2e65f84ad529f9c93a76fe)

---
+++ XrnCourse.BucketList/XrnCourse.BucketList/Domain/Validators/BucketItemValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using XrnCourse.BucketList.Domain.Models;
+
+namespace XrnCourse.BucketList.Domain.Validators
+{
+    public class BucketItemValidator : AbstractValidator<BucketItem>
+    {
+        public BucketItemValidator()
+        {
+            RuleFor(item => item.ItemDescription)
+                .NotEmpty()
+                .WithMessage("Description cannot be empty")
+                .Length(3, 50)
+                .WithMessage("Length must be between 3 and 50");
+
+        }
+    }
+}
--- XrnCourse.BucketList/XrnCourse.BucketList/Pages/SettingsPage.xaml.cs
+++ XrnCourse.BucketList/XrnCourse.BucketList/Pages/SettingsPage.xaml.cs
@@ -12,82 +12,9 @@ namespace XrnCourse.BucketList.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsPage : ContentPage
     {
-        IAppSettingsService settingsService;
-        IUsersService usersService;
-        IValidator userValidator;
-
         public SettingsPage()
         {
             InitializeComponent();
-
-            settingsService = new JsonAppSettingsService();
-            usersService = new JsonUsersService();
-            userValidator = new UserValidator();
-        }
-
-
-        protected async override void OnAppearing()
-        {
-            //get settings and intialize controls
-            var settings = await settingsService.GetSettings();
-            swEnableListSharing.On = settings.EnableListSharing;
-            swEnableNotifications.On = settings.EnableNotifications;
-
-            //get current User and intialize controls
-            var currentUser = await usersService.GetUser(settings.CurrentUserId);
-            txtUserName.Text = currentUser?.UserName;
-            txtEmail.Text = currentUser?.Email;
-
-            base.OnAppearing();
-        }
-        private async void BtnSaveSettings_Clicked(object sender, EventArgs e)
-        {
-            //save app settings
-            var currentSettings = await settingsService.GetSettings();
-            currentSettings.EnableListSharing = swEnableListSharing.On;
-            currentSettings.EnableNotifications = swEnableNotifications.On;
-            await settingsService.SaveSettings(currentSettings);
-
-            var user = await usersService.GetUser(currentSettings.CurrentUserId) ?? new User();
-
-            //save user info settings
-            user.UserName = txtUserName.Text.Trim();
-            user.Email = txtEmail.Text.Trim();
-
-            if (Validate(user))
-            {
-                await usersService.UpdateUser(user);
-
-                //close settings page
-                await Navigation.PopAsync();
-            }
-        }
-
-        /// <summary>
-        /// Validates the current values of a user object
-        /// </summary>
-        /// <param name="bucket"></param>
-        /// <returns></returns>
-        private bool Validate(User user)
-        {
-            errorEmail.Text = "";
-            errorUserName.Text = "";
-
-            var validationContext = new ValidationContext<User>(user);
-            var validationResult = userValidator.Validate(validationContext);
-            //loop through error to identify properties
-            foreach (var error in validationResult.Errors)
-            {
-                if (error.PropertyName == nameof(user.UserName))
-                {
-                    errorUserName.Text = error.ErrorMessage;
-                }
-                if (error.PropertyName == nameof(user.Email))
-                {
-                    errorEmail.Text = error.ErrorMessage;
-                }
-            }
-            return validationResult.IsValid;
         }
     }
 }
