b3e8b54b33a5118227dd55c2322de9e448fdc1e9
sigged <sderdeyn@gmail.com>
2019-11-12 06:47:23 +0100

    Add Web APi support using Service

--- XrnCourse.BucketList/XrnCourse.BucketList/App.xaml.cs
+++ XrnCourse.BucketList/XrnCourse.BucketList/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using FreshMvvm;
 using Xamarin.Forms;
 using XrnCourse.BucketList.Domain.Services;
+using XrnCourse.BucketList.Domain.Services.Api;
 using XrnCourse.BucketList.Domain.Services.Local;
 using XrnCourse.BucketList.ViewModels;
 
@@ -15,7 +16,7 @@ namespace XrnCourse.BucketList
             //register dependencies
             FreshIOC.Container.Register<IAppSettingsService>(new JsonAppSettingsService());
             FreshIOC.Container.Register<IUsersService>(new JsonUsersService());
-            FreshIOC.Container.Register<IBucketsService>(new JsonBucketsService());
+            FreshIOC.Container.Register<IBucketsService>(new ApiBucketsService());
 
             MainPage = new FreshNavigationContainer(FreshPageModelResolver.ResolvePageModel<MainViewModel>());
         }
