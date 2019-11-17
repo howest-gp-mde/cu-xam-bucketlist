5d99d0ce62f989484e49be0e2c142571509b3867
sigged <sderdeyn@gmail.com>
2019-11-17 16:51:25 +0100

    Allow connections from LAN on kestrel ports

--- XrnCourse.BucketList.WebApi/Migrations/20191117154954_Initial.cs
+++ XrnCourse.BucketList.WebApi/Migrations/20191117154954_Initial.cs
@@ -48,7 +48,6 @@ namespace XrnCourse.BucketList.WebApi.Migrations
                 {
                     Id = table.Column<Guid>(nullable: false),
                     BucketId = table.Column<Guid>(nullable: false),
-                    ParentBucketId = table.Column<Guid>(nullable: true),
                     ItemDescription = table.Column<string>(maxLength: 50, nullable: false),
                     CompletionDate = table.Column<DateTime>(nullable: true)
                 },
@@ -56,17 +55,48 @@ namespace XrnCourse.BucketList.WebApi.Migrations
                 {
                     table.PrimaryKey("PK_BucketlistItems", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_BucketlistItems_Bucketlists_ParentBucketId",
-                        column: x => x.ParentBucketId,
+                        name: "FK_BucketlistItems_Bucketlists_BucketId",
+                        column: x => x.BucketId,
                         principalTable: "Bucketlists",
                         principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "UserName" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), "siegfried@bucketlists.test", "Siegfried" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "UserName" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000002"), "deidre@bucketlists.test", "Deidre" });
+
+            migrationBuilder.InsertData(
+                table: "Bucketlists",
+                columns: new[] { "Id", "Description", "ImageUrl", "IsFavorite", "OwnerId", "Title" },
+                values: new object[] { new Guid("11111111-0000-0000-0000-000000000001"), "Advancing my programming skills", null, true, new Guid("00000000-0000-0000-0000-000000000001"), "Programming Dreams" });
+
+            migrationBuilder.InsertData(
+                table: "Bucketlists",
+                columns: new[] { "Id", "Description", "ImageUrl", "IsFavorite", "OwnerId", "Title" },
+                values: new object[] { new Guid("11111111-0000-0000-0000-000000000002"), "How I'm gonna spend the money earned from programming", null, true, new Guid("00000000-0000-0000-0000-000000000001"), "World Travels" });
+
+            migrationBuilder.InsertData(
+                table: "BucketlistItems",
+                columns: new[] { "Id", "BucketId", "CompletionDate", "ItemDescription" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-0000-0000-000000000001"), new Guid("11111111-0000-0000-0000-000000000001"), new DateTime(2019, 5, 17, 16, 49, 53, 777, DateTimeKind.Local).AddTicks(8522), "Become better in C#" },
+                    { new Guid("11111111-1111-0000-0000-000000000002"), new Guid("11111111-0000-0000-0000-000000000001"), new DateTime(2019, 10, 17, 16, 49, 53, 780, DateTimeKind.Local).AddTicks(6502), "Learn Xamarin" },
+                    { new Guid("11111111-1111-0000-0000-000000000003"), new Guid("11111111-0000-0000-0000-000000000001"), null, "Publish my first mobile app" },
+                    { new Guid("22222222-1111-0000-0000-000000000001"), new Guid("11111111-0000-0000-0000-000000000002"), null, "Hiking New Zealand" }
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_BucketlistItems_ParentBucketId",
+                name: "IX_BucketlistItems_BucketId",
                 table: "BucketlistItems",
-                column: "ParentBucketId");
+                column: "BucketId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Bucketlists_OwnerId",
